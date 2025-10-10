123ca35e7a7c8fc268d1db7147dda9fae5d93169
4thul_017 <122598894+Athul-krish@users.noreply.github.com>
2025-10-10 12:43:38 +0530

    Update linklist.c
    added ins_end and del-beg

--- linklist.c
+++ linklist.c
@@ -4,8 +4,8 @@ struct node{
 	int data;
 	struct node *next;
 };
-struct node *new,*head=NULL,*l;
-
+struct node *new,*head=NULL,*l,*t;
+int count=0;
 
 //creating a new node
 void create(){
@@ -29,8 +29,74 @@ else{
 	new->next=head;
 	head=new;
 }	
+	count++;
 }
 
+
+void ins_end(){
+create();
+if(head==NULL)
+{
+	head=new;
+}
+else{
+	l=head;
+	while(l->next!=NULL)
+	{
+		l=l->next;
+		}
+	l->next=new;
+	count++;
+}
+}
+
+
+void ins_pos(){
+int i,pos;
+printf("Enter position to be inserted:");
+scanf("%d",&pos);
+if(pos==1){
+	ins_beg();
+	}
+else if(pos==count+1)
+	{
+	ins_end();
+	}
+else if(pos>count+1)
+	{
+	printf("Invalid postion");
+	}
+else
+{
+	create();
+	l=head;
+for(i=0;i<pos-2;i++)
+	{
+		l=l->next;
+		}
+	new->next=l->next;
+	l->next=new;
+	count++;
+}	
+}
+	
+	
+	
+void del_beg(){
+if(head==NULL){
+	printf("List empty");
+	}
+else{
+	t=head;
+	head=head->next;
+	free(t);
+	count--;
+	}
+	printf("\nDeleted first element\n");
+}
+void del_end(){}
+void del_pos(){}
+	
 void disp(){
 if(head==NULL){
 	printf("List is empty");
@@ -45,9 +111,15 @@ else{
 }
 
 void main(){
-ins_beg();
-ins_beg();
-ins_beg();
+ins_end();
+ins_end();
+ins_end();
+ins_end();
+ins_end();
+disp();
+
+
+
+del_beg();
 disp();
 }
-
