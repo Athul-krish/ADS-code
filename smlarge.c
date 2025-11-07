b42dca79aae5d204020666621854ecdc5c541b77
4thul_017 <122598894+Athul-krish@users.noreply.github.com>
2025-11-07 11:59:26 +0530

    Add files via upload

---
+++ smlarge.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(){
+int a[100],i,n,lrg,sml;
+printf("Enter array limit:");
+scanf("%d",&n);
+printf("Enter array elements:");
+for(i=0;i<n;i++)
+ {
+ scanf("%d",&a[i]);
+ }
+lrg=a[0];
+sml=a[0];
+printf("Array is:\n");
+for(i=0;i<n;i++)
+ {
+ printf("%d ",a[i]);
+ }
+ 
+if(a[i]>lrg)
+{
+	lrg=a[i];
+ }
+if(a[i]<sml)
+{
+	sml=a[i];	 
+ }
+ printf("\n Largest:%d  Smallest:%d",lrg,sml);
+}
